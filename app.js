fb500626f899e16cd20d96ed72b3c3c448a9d039
omkar_nath <omkarnath1123@gmail.com>
2019-05-04 22:04:37 +0530

    redirect app to offline is network is down

--- app.js
+++ app.js
@@ -4,21 +4,33 @@ const url = require("url");
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+let BASE_PAGE = "UI/pages/home/home.html";
 
-app.on("ready", () => {
+app.on("ready", async () => {
+  await new Promise(function(resolve, reject) {
+    require("dns").resolve("www.google.com", function(err) {
+      if (err) {
+        BASE_PAGE = "UI/pages/offline/offline.html";
+      } else {
+        BASE_PAGE = "UI/pages/home/home.html";
+      }
+      resolve();
+    });
+  });
+
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-     minWidth: 1200,
-     minHeight: 800,
-    titleBarStyle: 'hidden',
+    minWidth: 1200,
+    minHeight: 800,
+    // titleBarStyle: 'hidden',
     // fullscreen: true,
     icon: path.join(__dirname, "images/icon.icns")
   });
   // mainWindow.setFullScreen(true);
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, "UI/pages/home/home.html"),
+      pathname: path.join(__dirname, BASE_PAGE),
       protocol: "file",
       slashes: true
     })
